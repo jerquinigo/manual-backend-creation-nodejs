ae10000d85594bb2dd08e78f315073012d41a316
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-09-11 18:30:01 -0400

    adding changes to app.js

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get("/", (req, res, next) => {
+  res.send("this is the homepage");
+});
+
+app.get("*", (req, res, next) => {
+  res.send("error");
+});
+
 app.listen(3100, () => {
   console.log("port 3100");
 });
